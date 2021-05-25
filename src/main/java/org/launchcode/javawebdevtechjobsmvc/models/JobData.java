010fb0413855519f8635e1df8652b1bb92e94c8d
rlp390 <rlp390@hotmail.com>
2021-05-25 09:25:07 -0500

    Display List Jobs Completed

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
@@ -29,7 +29,6 @@ public class JobData {
      * Fetch list of all job objects from loaded data,
      * without duplicates, then return a copy.
      */
-
     public static ArrayList<Job> findAll() {
 
         // load data, if not already loaded
