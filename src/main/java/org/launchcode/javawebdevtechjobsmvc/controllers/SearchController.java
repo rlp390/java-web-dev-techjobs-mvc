6275c594c34f0a15bc9a581661b0eb09d0031ee1
rlp390 <rlp390@hotmail.com>
2021-05-25 09:57:47 -0500

    Added search control post handler and updated search.html list

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -19,10 +20,30 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
+        String checkedBox = "all";
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("checkedBox", checkedBox);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        String checkedBox = searchType;
+        if(searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns",columnChoices);
+        model.addAttribute("checkedBox", checkedBox);
+
+        return "search";
+    }
+
 
 }
